6b057b9235da4ab6217b6000d94b089980860976
Yorick <yorick-1996@hotmail.com>
2018-01-15 20:40:03 +0100

    OK

--- src/main/java/be/helha/groupeB7/controller/EvenementController.java
+++ src/main/java/be/helha/groupeB7/controller/EvenementController.java
@@ -12,11 +12,25 @@ import be.helha.groupeB7.sessionejb.GestionEvenementEJB;
 @Named
 @RequestScoped
 public class EvenementController {
+	private Evenement event;
 
 	@EJB
 	private GestionEvenementEJB gestionEvenementEJB;
 	
 	public List<Evenement> doListEvent(){
 		return gestionEvenementEJB.selectAll();
+	}
+	
+	public String goDetailEvent(Evenement event) {
+		this.event = event;
+		return "detailEvenement.xhtml";
+	}
+
+	public Evenement getEvent() {
+		return event;
+	}
+
+	public void setEvent(Evenement event) {
+		this.event = event;
 	}
 }
